65308304d017ec31af44377fd3477d5d22f953b7
hseritt <harlin.seritt@gmail.com>
2017-08-17 16:20:27 -0600

    Initial commit

---
+++ settings/alfresco-global.properties.ts
@@ -0,0 +1,10 @@
+
+### Transformation Server ###
+transformserver.aliveCheckTimeout=2
+transformserver.test.cronExpression=0/10 * * * * ?
+transformserver.disableSSLCertificateValidation=false
+transformserver.username=alfresco
+transformserver.password=alfresco
+transformserver.qualityPreference=QUALITY
+transformserver.transformationTimeout=300
+transformserver.url=http://192.168.56.102:8080/transformation-server
